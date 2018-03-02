78fad029ff03b5559c88662a4486cdf17f9ae9db
Arnau <arnau.canyadell@est.fib.upc.edu>
2018-03-02 15:14:24 -0600

    added gitignore

---
+++ plane_segmentation_tutorial/build/CMakeFiles/plane_segmentation_tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plane_segmentation_tutorial.dir/src/plane_segmentation_tutorial.cpp.o"
+  "devel/lib/plane_segmentation_tutorial/plane_segmentation_tutorial.pdb"
+  "devel/lib/plane_segmentation_tutorial/plane_segmentation_tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plane_segmentation_tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ plane_segmentation_tutorial/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/arnau/catkin_ws/src/plane_segmentation_tutorial
+# Build directory: /home/arnau/catkin_ws/src/plane_segmentation_tutorial/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
